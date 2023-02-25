f6d791ff75ebebdfb3eac438f1a47ffa6b877a74
artir <artir@REYANSH2021>
2023-02-25 11:37:24 +0530

    first stringManipulation program

--- StringMnipulation1/program1.cs
+++ StringMnipulation1/program1.cs
@@ -10,10 +10,25 @@ namespace StringMnipulation1
     {
 
         public static void Main()
-
-
         {
 
+            StringManipulation();
+
+            ShowFullName();
+
+            CalculateLenghtOfString();
+
+            CalculateVowels();
+;
+            
+
+        }
+         static void StringManipulation()
+        {
+            Console.WriteLine("Inside of the StringManipulation Function");
+        }
+        static void ShowFullName()
+        {
             //SHOW FULL NAME
 
             string FirstName, MiddleName, LastName, FullName;
@@ -32,39 +47,43 @@ namespace StringMnipulation1
             FullName = FirstName + " " + MiddleName + " " + LastName;
 
             Console.WriteLine(FullName);
+        }
 
+        static void CalculateLenghtOfString()
+        {
             //calculate length of the string
 
-            //Console.WriteLine("Please enter any string");
-            //var str = Console.ReadLine();  //Welcome
+            Console.WriteLine("Please enter any string");
+            var str = Console.ReadLine();  //Welcome
 
-            //int counter = 0;
+            int counter = 0;
 
-            //foreach (var item in str)
-            //{
-            //    counter++;
-            //}
-            //Console.WriteLine("length of the string =" + counter);
+            foreach (var item in str)
+            {
+                counter++;
+            }
+            Console.WriteLine("length of the string =" + counter);
+        }
 
-
+        static void CalculateVowels()
+        {
             //calculate the vowels of the string
 
-            //Console.WriteLine("please enter any  string");
-            //var str = Console.ReadLine();  //welcome
+            Console.WriteLine("please enter any  string");
+            var str = Console.ReadLine();  //welcome
 
-            //int VowelsCountr = 0;
-            //foreach (var item in str)
-            //{
+            int VowelsCountr = 0;
+            foreach (var item in str)
+            {
 
-            //    if (item == 'a' || item == 'e' || item == 'i' || item == 'o' || item == 'u' ||
-            //        item == 'A' || item == 'E' || item == 'I' || item == 'O' || item == 'U')
-            //    {
-            //        VowelsCountr++;
-            //    }
-            //}
+                if (item == 'a' || item == 'e' || item == 'i' || item == 'o' || item == 'u' ||
+                    item == 'A' || item == 'E' || item == 'I' || item == 'O' || item == 'U')
+                {
+                    VowelsCountr++;
+                }
+            }
 
-            //Console.WriteLine("length of the string=" + VowelsCountr);
-
+            Console.WriteLine("length of the string=" + VowelsCountr);
 
         }
     }
