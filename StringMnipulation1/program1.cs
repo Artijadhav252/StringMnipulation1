e94f9cde1de9c8f6155a196bb5479179a19fe1ef
artir <artir@REYANSH2021>
2023-02-25 11:48:39 +0530

    First StringManipulation

--- StringMnipulation1/program1.cs
+++ StringMnipulation1/program1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,11 +20,11 @@ namespace StringMnipulation1
             CalculateLenghtOfString();
 
             CalculateVowels();
-;
-            
+            ;
+
 
         }
-         static void StringManipulation()
+        static void StringManipulation()
         {
             Console.WriteLine("Inside of the StringManipulation Function");
         }
